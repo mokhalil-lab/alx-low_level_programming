0d11bf978e8fa4510c6af21857aa2b8427acad43
mokhalil <mo7ammedkhalil@gmail.com>
2023-03-16 18:10:49 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - C program that prints a line using puts function
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
