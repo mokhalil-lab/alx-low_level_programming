d9aaab467db1c33cfd4d84e49bdc1350bf890adc
mokhalil <mo7ammedkhalil@gmail.com>
2023-03-23 09:12:46 -0700

    adding description

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 /**
-*while (i < 10)
+*comment out infinite loop while (i < 10)
 *{
 *putchar(i);
-*}
+*} printf indicate the overcoming of the blackhole
 */
 printf("Infinite loop avoided! \\o/\n");
 
