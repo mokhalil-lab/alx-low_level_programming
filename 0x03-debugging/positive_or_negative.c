27195ff92b3c445a9307675ef03ea36f494a9f68
mokhalil <mo7ammedkhalil@gmail.com>
2023-03-23 08:25:10 -0700

    test the function positive_or_negative() gives the correct output when given a case of 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+	printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+	printf("%d is zero\n", i);
+	}
+	else
+	{
+	printf("%d is negative\n", i);
+	}
+}
