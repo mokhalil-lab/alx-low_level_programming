85b153a5b4641cc58e57980608407d7a5733dda1
mokhalil <mo7ammedkhalil@gmail.com>
2023-03-30 08:56:07 -0700

    function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strncpy - copy a string
+* @dest: input value
+* @src: input value
+* @n: input value
+*
+* Return: dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int j;
+
+for (j = 0 ; j < n && src[j] != '\0' ; j++)
+{
+dest[j] = src[j];
+}
+
+dest[j] = '\0';
+return (dest);
+}
