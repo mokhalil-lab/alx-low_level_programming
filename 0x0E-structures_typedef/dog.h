69cf25173500d5168a3ca0bac612a035f710acea
mokhalil <mo7ammedkhalil@gmail.com>
2023-04-17 21:50:32 -0700

    betty fixs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+* struct dog - a dog's basic info
+* @name: First member
+* @age: Second member
+* @owner: Third member
+*/
+
+
+struct dog
+{
 
 char *name;
 float age;
